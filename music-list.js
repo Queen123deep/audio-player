a8b10c228fc6e07359637ccc65ad7396ace436d2
Kanwal <110118296+Queen123deep@users.noreply.github.com>
2022-09-18 12:17:34 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -3,8 +3,8 @@ let allMusic = [
   {
     name: "Pent Straight",
     artist: "Gurnam Bhullar",
-    img: "./image/images (8).jpeg",
-    src: "./music/Pent Straight (RiskyjaTT.CoM)"
+    img: "./images/images (8).jpeg",
+    src: "./music/Pent Straight (RiskyjaTT.CoM).mp3"
   },
   
 ];
