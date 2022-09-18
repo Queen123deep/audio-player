a6cf52f5e306dc80a5ab18f6160a95656b7d7b57
Kanwal <110118296+Queen123deep@users.noreply.github.com>
2022-09-18 17:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,29 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/',
-        name : '',
-        artist : '',
-        music : 'music/Churi_1.mp3'
+        img : 'images/stay.png',
+        name : 'Stay',
+        artist : 'The Kid LAROI, Justin Bieber',
+        music : 'music/stay.mp3'
     },
-  
+    {
+        img : 'images/fallingdown.jpg',
+        name : 'Falling Down',
+        artist : 'Wid Cards',
+        music : 'music/fallingdown.mp3'
+    },
+    {
+        img : 'images/faded.png',
+        name : 'Faded',
+        artist : 'Alan Walker',
+        music : 'music/Faded.mp3'
+    },
+    {
+        img : 'images/ratherbe.jpg',
+        name : 'Rather Be',
+        artist : 'Clean Bandit',
+        music : 'music/Rather Be.mp3'
+    }
 ];
 
 loadTrack(track_index);
